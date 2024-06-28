de1cb4dec1155778e4ebea544db064b691d80d69
Muhammad Salman <m.salman181920@gmail.com>
2024-06-28 14:32:39 +0500

    Update app_exceptions.dart

--- lib/data/app_exceptions.dart
+++ lib/data/app_exceptions.dart
@@ -7,3 +7,21 @@ class AppExceptions implements Exception {
     return '$_msg $_prefix';
   }
 }
+
+class FetchDataException extends AppExceptions {
+  FetchDataException([String? message])
+      : super(message, "Error during communication");
+}
+
+class BadRequestException extends AppExceptions {
+  BadRequestException([String? message]) : super(message, "Invalid request");
+}
+
+class UnauthorizedException extends AppExceptions {
+  UnauthorizedException([String? message])
+      : super(message, "Unauthorized request");
+}
+
+class InvalidInputException extends AppExceptions {
+  InvalidInputException([String? message]) : super(message, "Invalid input");
+}
